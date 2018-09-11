1f561396bd3497f4325f5fbb06065ab4983c3774
Burak Akin <burakakin@buraks.local>
2018-09-12 01:11:47 +0300

    First AR project

--- First AR/ViewController.swift
+++ First AR/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    var x :Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        configuration.planeDetection = [.horizontal,.vertical]
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addFunc(_ sender: UIButton) {
+        x = x + 0.1
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.2)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        node.position = SCNVector3(x,0,0)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
     }
-
+    
+//    func restartSession(){
+//        self.sceneView.session.pause()
+//        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+//            node.removeFromParentNode()
+//        }
+//        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+//        self.sceneView.session.run(configuration)
+//    }
 
 }
+
 
